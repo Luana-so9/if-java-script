1e4710308385ee008b18aa06d56efaa0510da6fa
Luana-so9 <luanaso2018@gmail.com>
2026-05-08 17:20:41 -0300

    Add files via upload

---
+++ if.html/script.js
@@ -0,0 +1,51 @@
+
+
+
+let variavel = 10;
+if (variavel == 10)
+{
+    alert("A variavel vale 10");
+}
+
+let variavel2="Luana";
+if(variavel2=="Luana")
+{
+    alert("meu nome é LUANA");
+}
+
+
+ function funcao2(){
+    let digitou = document.getElementById("caixaok").value;
+    if(digitou == "ok")
+    {
+        alert("Você digitou ok");
+    }  
+ }
+  function text(){
+    let verificou= document.getElementById("numero5").value
+    if(verificou==5)
+        {
+        alert("você acabou de confirmar seu 5");
+        }
+  }
+
+  function paragrafo(){
+    let admin=document.getElementById("correto").value;
+    if(admin =="admin")
+    {
+        alert("o paragráfo mudou para CORRETO");
+    }
+  }
+
+  function cor(){
+    let cor=document.getElementById("mudarCor").value;
+    if (cor=="violeta")
+            {  
+                document.getElementById("caixa").style.backgroundColor="#965ca0";
+  }
+  }   
+
+
+  function azul(){
+    0
+  }
